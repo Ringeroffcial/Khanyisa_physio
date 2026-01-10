71383ec0ade10f348461815ef331e1118dc657fb
Ringeroffcial <166762261+Ringeroffcial@users.noreply.github.com>
2026-01-10 17:44:43 +0200

    first push

--- khanyisa_physiotherapy/src/App.jsx
+++ khanyisa_physiotherapy/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import Header from '../../components/Header';
-import Hero from '../../components/Hero';
-import Services from '../../components/Services';
-import Contact from '../../components/Contact';
-import Footer from '../../components/Footer';
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Services from '../components/Services';
+import Contact from '../components/Contact';
+import Footer from '../components/Footer';
 import './App.css';
 
 const App = () => {
